bb5d01a58498b37e911cf908daf179f404fa8a50
ATANASOV <mladenatanasov75@gmail.com>
2019-03-05 15:09:12 +0100

    tp version 0.0.1

---
+++ src/math.test.js
@@ -0,0 +1,54 @@
+const Util = require('./math');
+
+describe('Factorial', function () {
+    test.each([
+        [0, 1],
+        [1, 1],
+        [2, 2],
+        [3, 6],
+        [4, 24],
+        [5, 120],
+    ])(
+        'Factorial %i equals to %i',
+        (n, expected) => {
+            expect(Util.factorial(n)).toBe(expected);
+        },
+    );
+    test('Factorial of negative integers throws exception', () => {
+        expect(()=> {Util.factorial(-10)}).toThrow();
+    });
+});
+
+
+describe('IsPrimerial', function () {
+    test.each([
+        [0, false],
+        [1, false],
+        [2, true],
+        [3, true],
+        [4, false],
+        [5, true],
+        [6, false],
+    ])(
+        'IsPrime for %i is %p',
+        (n, expected) => {
+            expect(Util.isPrime(n)).toBe(expected);
+        },
+    );
+
+});
+
+
+describe('Sum Primerial', function () {
+    test.each([
+        [6,10],
+        [8, 17],
+    ])(
+        'IsPrime for %i is %p',
+        (n, expected) => {
+            expect(Util.sumPrime(n)).toBe(expected);
+        },
+    );
+
+});
+
